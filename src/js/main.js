7287ce810d929615959cc728c25ef091bee03214
yoichi kobayashi <info@tplh.net>
2017-08-04 16:53:07 +0900

    [add] Hover class for mouseover effects.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 import ScrollManager from './modules/SmoothScrollManager'
+import Hover from 'js-util/Hover.js';
 
 const init = () => {
+  const elmHover = document.getElementsByClassName('js-hover');
+  for (var i = 0; i < elmHover.length; i++) {
+    new Hover(elmHover[i]);
+  }
+
   const scrollManager = new ScrollManager();
   scrollManager.start();
 }
