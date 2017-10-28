470db285b65a91bc8257021d1de028cce94b0f34
yoichi kobayashi <info@tplh.net>
2017-10-28 23:11:05 +0900

    [update] changed default value of acceleration calculation on Hookes class.

--- src/js/modules/smooth_scroll_manager/Hookes.js
+++ src/js/modules/smooth_scroll_manager/Hookes.js
@@ -6,7 +6,7 @@ export default class Hookes {
     this.acceleration = [0, 0, 0];
     this.anchor = [0, 0, 0];
     this.k = (opt && opt.k !== undefined) ? opt.k : 0.3;
-    this.d = (opt && opt.d !== undefined) ? opt.d : 0.8;
+    this.d = (opt && opt.d !== undefined) ? opt.d : 0.7;
     this.m = (opt && opt.m !== undefined) ? opt.m : 1;
   }
   render() {
