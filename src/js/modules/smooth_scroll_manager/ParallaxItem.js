effbe9399a98d40d51a9a643e352e5a086fcf5b9
yoichi kobayashi <info@tplh.net>
2017-11-04 04:20:04 +0900

    [update] max and min attributes ware  replaced to range attribute.

--- src/js/modules/smooth_scroll_manager/ParallaxItem.js
+++ src/js/modules/smooth_scroll_manager/ParallaxItem.js
@@ -7,12 +7,10 @@ export default class ParallaxItem {
     this.elm = elm;
     this.height = 0;
     this.top = 0;
-    this.maxX = (opt && opt.maxX) ? opt.maxX : 10000;
-    this.minX = (opt && opt.minX) ? opt.minX : -10000;
+    this.rangeX = (opt && opt.rangeX) ? opt.rangeX : 10000;
     this.ratioX = (opt && opt.ratioX) ? opt.ratioX : 0;
     this.unitX = (opt && opt.unitX) ? opt.unitX : 'px';
-    this.maxY = (opt && opt.maxY) ? opt.maxY : 10;
-    this.minY = (opt && opt.minY) ? opt.minY : -10;
+    this.rangeY = (opt && opt.rangeY) ? opt.rangeY : 10;
     this.ratioY = (opt && opt.ratioY) ? opt.ratioY : 0.012;
     this.unitY = (opt && opt.unitY) ? opt.unitY : '%';
   }
@@ -21,17 +19,18 @@ export default class ParallaxItem {
     this.height = rect.height;
     this.top = scrollTop + rect.top;
     this.elm.style.backfaceVisibility = 'hidden';
+    this.render();
   }
   render(iwWorking) {
     const x = (iwWorking) ? MathEx.clamp(
       this.hookes.velocity[0] * this.ratioX,
-      this.minX,
-      this.maxX
+      this.rangeX * -1,
+      this.rangeX
     ) : 0;
     const y = (iwWorking) ? MathEx.clamp(
       (this.hookes.velocity[1] - (this.top + this.height * 0.5)) * this.ratioY,
-      this.minY,
-      this.maxY
+      this.rangeY * -1,
+      this.rangeY
     ) : 0;
     this.elm.style.transform = `translate3D(${x}${this.unitX}, ${y}${this.unitY}, 0)`;
   }
