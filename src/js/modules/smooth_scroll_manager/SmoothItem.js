9c3fdf4070a0df3d2567e2e48188e5fadc406a25
yoichi kobayashi <info@tplh.net>
2018-02-23 01:03:57 +0900

    SmoothItem class became to render translate x.

--- src/js/modules/smooth_scroll_manager/SmoothItem.js
+++ src/js/modules/smooth_scroll_manager/SmoothItem.js
@@ -7,6 +7,9 @@ export default class SmoothItem {
     this.elm = elm;
     this.height = 0;
     this.top = 0;
+    this.rangeX = (opt && opt.rangeX) ? opt.rangeX : null;
+    this.ratioX = (opt && opt.ratioX) ? opt.ratioX : 0.0;
+    this.unitX = (opt && opt.unitX) ? opt.unitX : 'px';
     this.rangeY = (opt && opt.rangeY) ? opt.rangeY : null;
     this.ratioY = (opt && opt.ratioY) ? opt.ratioY : 0.1;
     this.unitY = (opt && opt.unitY) ? opt.unitY : 'px';
@@ -21,10 +24,11 @@ export default class SmoothItem {
     let x = 0;
     let y = 0;
     if (isWorking) {
+      x = this.hookes.velocity[1] * this.ratioX;
       y = this.hookes.velocity[1] * this.ratioY;
-      if (Math.abs(this.hookes.acceleration[1]) < 0.01) this.hookes.velocity[1] = this.hookes.anchor[1];
+      if (this.rangeX) x = MathEx.clamp(x, -this.rangeX, this.rangeX);
       if (this.rangeY) y = MathEx.clamp(y, -this.rangeY, this.rangeY);
     }
-    this.elm.style.transform = `translate3D(0, ${y}${this.unitY}, 0)`;
+    this.elm.style.transform = `translate3D(${x}${this.unitX}, ${y}${this.unitY}, 0)`;
   }
 }
