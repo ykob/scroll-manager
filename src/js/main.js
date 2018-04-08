f8a517ecec4dac606067ca1535f86eb18233086b
yoichi kobayashi <info@tplh.net>
2018-04-08 17:09:08 +0900

    load a stylesheet ansynchronously.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
+const viewportUnitsBuggyfill = require('viewport-units-buggyfill');
+
 const pageId = document.querySelector('.l-page').getAttribute('data-page-id');
+const ua = window.navigator.userAgent;
+const link = document.querySelector('link[as=style]');
 
 const init = () => {
   switch (pageId) {
@@ -11,4 +15,14 @@ const init = () => {
     default:
   }
 }
-init();
+
+// preload stylesheet other than Google Chrome browser.
+if (ua.indexOf('Chrome') < 0) link.rel = 'stylesheet';
+
+setTimeout(() => {
+  // Making viewport units (vh|vw|vmin|vmax) work properly in Mobile Safari.
+  viewportUnitsBuggyfill.init();
+
+  // run initialize function.
+  init();
+}, 100);
