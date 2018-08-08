f473bbcaab11788e697740671d04354f901e0684
yoichi kobayashi <info@tplh.net>
2018-08-08 13:53:12 +0900

    changed init function to async function.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,22 @@
+require("babel-polyfill");
+
 const viewportUnitsBuggyfill = require('viewport-units-buggyfill');
+const sleep = require('js-util/sleep');
 
 const pageId = document.querySelector('.l-page').getAttribute('data-page-id');
 const ua = window.navigator.userAgent;
 const link = document.querySelector('link[as=style]');
 
-const init = () => {
+const init = async () => {
+  // preload stylesheet other than Google Chrome browser.
+  if (ua.indexOf('Edge') > -1 || ua.indexOf('Chrome') < 0) link.rel = 'stylesheet';
+
+  // Making viewport units (vh|vw|vmin|vmax) work properly in Mobile Safari.
+  viewportUnitsBuggyfill.init();
+
+  await sleep(100);
+
+  // run initialize function.
   switch (pageId) {
     case 'index':
       require ('./init/index.js').default();
@@ -15,14 +27,4 @@ const init = () => {
     default:
   }
 }
-
-// preload stylesheet other than Google Chrome browser.
-if (ua.indexOf('Chrome') < 0) link.rel = 'stylesheet';
-
-setTimeout(() => {
-  // Making viewport units (vh|vw|vmin|vmax) work properly in Mobile Safari.
-  viewportUnitsBuggyfill.init();
-
-  // run initialize function.
-  init();
-}, 100);
+init();
