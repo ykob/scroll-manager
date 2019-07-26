fa3bef51a3d89b67cbed67ebaffca7718457f767
yoichi kobayashi <info@tplh.net>
2019-07-26 17:10:07 +0900

    refactored loadContentImgs function.

--- src/js/modules/common/loadContentImgs.js
+++ src/js/modules/common/loadContentImgs.js
@@ -1,14 +1,10 @@
 import loadImgs from './loadImgs';
 
 export default async function(contents, addImgs = []) {
-  const imgs = contents.querySelectorAll('img');
-  const imgArr = [];
-
-  for (var i = 0; i < imgs.length; i++) {
-    imgArr[i] = imgs[i].src;
-  }
-  for (var j = 0; j < addImgs.length; j++) {
-    imgArr.push(addImgs[j]);
-  }
-  return loadImgs(imgArr);
+  return loadImgs([
+    ...[...contents.querySelectorAll('img')].map(img => {
+      return img.src;
+    }),
+    ...addImgs,
+  ]);
 }
