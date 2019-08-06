db181337256b07d85c35fce641c8a04ba79911aa
yoichi kobayashi <info@tplh.net>
2019-08-06 23:48:11 +0900

    refactored the process to create some instance on the initialize function.

--- src/js/modules/scroll_manager/ScrollItems.js
+++ src/js/modules/scroll_manager/ScrollItems.js
@@ -6,15 +6,9 @@ export default class ScrollItems {
     this.scrollItems = [];
   }
   init(wrap) {
-    const elmScrollItems = wrap.querySelectorAll('.js-scroll-item');
-
-    this.scrollItems = [];
-
-    for (var i = 0; i < elmScrollItems.length; i++) {
-      this.scrollItems[i] = new ScrollItem(
-        elmScrollItems[i], this.scrollManager
-      );
-    }
+    this.scrollItems = [...wrap.querySelectorAll('.js-scroll-item')].map(o => {
+      return new ScrollItem(o, this.scrollManager);
+    });
   }
   scroll() {
     for (var i = 0; i < this.scrollItems.length; i++) {
